96f95a4c091430c39bd93719ff7f42ef1567ea4a
Viggy12126 <vighnesh2607@gmail.com>
2024-03-28 12:08:51 -0400

    injected into ejs template

---
+++ index.js
@@ -0,0 +1,67 @@
+var expess=require("express");
+
+var app=expess();
+var bodyParser=require("body-parser");
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+
+const PORT=3000;
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',async (req,res)=>{
+
+
+try {
+   
+    var currSearch = "Sandwich";
+
+
+    const APP_ID = "72b759e9";
+    const APP_KEY = "91ca6d8205ca79ceb94a852ea8d81ad5";
+
+  
+    const url = `https://api.edamam.com/search?q=${currSearch}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+   
+    res.render("recipe", { data: data.hits, currSearch:currSearch }); 
+} catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Error fetching data. Please try again later.");
+}
+})
+
+app.post("/search", async (req, res) => {
+    const { search } = req.body; 
+    console.log(search);
+
+
+    const APP_ID = "72b759e9";
+    const APP_KEY = "91ca6d8205ca79ceb94a852ea8d81ad5";
+    const url = `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+   
+
+        res.render("recipe", { data: data.hits, currSearch:search
+         });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send("Error fetching data. Please try again later.");
+    }
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is listenting on PORT ${PORT}`);
+})
